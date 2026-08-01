655d77ed660b8e6506ab0112a9deaa44095eec65
Fiantso Harena <fiantsorav@gmail.com>
2026-08-01 16:15:48 +0300

    feat(cli): announce check in the launch demo
    The demo now plays a queen raid ending in Qxe5+ and reports the check,
    exercising attack detection through the public rules API.

--- src/main/java/org/saitama/Main.java
+++ src/main/java/org/saitama/Main.java
@@ -5,21 +5,31 @@ import org.saitama.board.Move;
 import org.saitama.board.Position;
 import org.saitama.board.Square;
 import org.saitama.fen.Fen;
+import org.saitama.rules.Attacks;
 
 /** Command-line entry point of the chess engine. */
 public class Main {
   static void main() {
     Position position = Fen.parse(Fen.STARTING);
-    List<Move> opening =
+    List<Move> game =
         List.of(
             new Move.Normal(Square.E2, Square.E4),
-            new Move.Normal(Square.C7, Square.C5),
-            new Move.Normal(Square.G1, Square.F3));
-    for (Move move : opening) {
+            new Move.Normal(Square.E7, Square.E5),
+            new Move.Normal(Square.D1, Square.H5),
+            new Move.Normal(Square.E8, Square.E7),
+            new Move.Normal(Square.H5, Square.E5));
+    for (Move move : game) {
       position = position.apply(move);
     }
     IO.println(position.board());
-    IO.println("After 1. e4 c5 2. Nf3");
-    IO.println(Fen.write(position));
+    IO.println("After 1. e4 e5 2. Qh5 Ke7 3. Qxe5+");
+    IO.println(
+        switch (position.sideToMove()) {
+          case WHITE -> "White to move";
+          case BLACK -> "Black to move";
+        });
+    if (Attacks.isInCheck(position)) {
+      IO.println("Check!");
+    }
   }
 }
