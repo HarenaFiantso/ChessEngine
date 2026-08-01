42d7bb1d5d61ab29d32b7da858b437ab7c8a7add
Fiantso Harena <fiantsorav@gmail.com>
2026-08-01 15:50:49 +0300

    feat(cli): play an opening sequence on launch
    Launching the engine now applies 1. e4 c5 2. Nf3 move by move and
    prints the resulting board and FEN record, exercising move application
    end to end through the same API future search code will use.

--- src/main/java/org/saitama/Main.java
+++ src/main/java/org/saitama/Main.java
@@ -1,17 +1,25 @@
 package org.saitama;
 
+import java.util.List;
+import org.saitama.board.Move;
 import org.saitama.board.Position;
+import org.saitama.board.Square;
 import org.saitama.fen.Fen;
 
 /** Command-line entry point of the chess engine. */
 public class Main {
   static void main() {
     Position position = Fen.parse(Fen.STARTING);
+    List<Move> opening =
+        List.of(
+            new Move.Normal(Square.E2, Square.E4),
+            new Move.Normal(Square.C7, Square.C5),
+            new Move.Normal(Square.G1, Square.F3));
+    for (Move move : opening) {
+      position = position.apply(move);
+    }
     IO.println(position.board());
-    IO.println(
-        switch (position.sideToMove()) {
-          case WHITE -> "White to move";
-          case BLACK -> "Black to move";
-        });
+    IO.println("After 1. e4 c5 2. Nf3");
+    IO.println(Fen.write(position));
   }
 }
